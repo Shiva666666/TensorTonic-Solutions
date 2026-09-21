7850b80566af8e930899b58248c4105c7482ddba
Shiva666666 <155778248+Shiva666666@users.noreply.github.com>
2026-09-21 20:45:32 +0530

    Solve make-diagonal

---
+++ make-diagonal/make-diagonal.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def make_diagonal(v: list) -> np.ndarray:
+    """
+    Returns a NumPy array with shape (N, N).
+    """
+    # Write code here
+
+    arr = np.diag(v)
+    return arr
+    pass
